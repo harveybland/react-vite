eaa850697cb4649a8362b66773fedccf8a84dbb6
harveybland <harvey.bland@genius.online>
2023-02-17 15:27:46 +0000

    show array of data

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -2,16 +2,22 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [user, setuser] = useState('');
+  const [title, setTitle] = useState('');
 
-  fetch('https://jsonplaceholder.typicode.com/users')
+  fetch('https://jsonplaceholder.typicode.com/posts')
     .then((res) => res.json())
     .then((data: any) => {
       console.log(data);
-      setuser(data[0].name);
-    });
+      // setTitle(data[0].title);
 
-  return <div>User : {user}</div>;
+      return (
+        <div>
+          {data.map((item: any) => {
+            return <span>{item.title}</span>;
+          })}
+        </div>
+      );
+    });
 }
 
 export default App;
